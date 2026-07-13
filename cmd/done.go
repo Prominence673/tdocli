48c93f4800566b3ff3550999c4b470d549829faa
Lautaro <safarakiri@gmail.com>
2026-07-13 16:43:49 +0000

    feat(done): add command to mark task as completed
    Usage: tasker done <id>

---
+++ cmd/done.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"fmt"
+	"tdocli/task"
+	"strconv"
+)
+
+var doneCmd = &cobra.Command{
+	Use:   "done <id>",
+	Short: "Mark a task as done",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		id, err := strconv.Atoi(args[0])
+		if err != nil {
+			return err
+		}
+		err = task.MarkCompleted(id)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Task %d marked as done\n", id)
+		return nil
+	},
+}
+
+func init(){
+	rootCmd.AddCommand(doneCmd)
+}
