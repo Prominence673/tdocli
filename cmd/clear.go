8a24a8f7099146d853aee38be25b00627401da56
Lautaro <safarakiri@gmail.com>
2026-07-13 16:43:49 +0000

    feat(clear): add command to wipe all tasks
    Usage: tasker clear

---
+++ cmd/clear.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"fmt"
+	"tdocli/task"
+)
+
+var clearCmd = &cobra.Command{
+	Use:   "clear",
+	Short: "Clear all tasks",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := task.Clear()
+		if err != nil {
+			return err
+		}
+		fmt.Println("All tasks cleared")
+		return nil
+	},
+}
+
+func init(){
+	rootCmd.AddCommand(clearCmd)
+}
